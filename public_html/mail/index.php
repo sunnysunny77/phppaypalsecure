d46e4b1f73015332e2402a46a2ff5be25ad5080c
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-08-20 16:48:28 +0800

    Update index.php

--- public_html/mail/index.php
+++ public_html/mail/index.php
@@ -3,10 +3,10 @@
 session_start();
 if ($_SESSION['token'] === $_POST['token']) {
     require "../../keys.php";
-    $token = $_SERVER['HTTP_HOST'];
+    $host = $_SERVER['HTTP_HOST'];
     $token = $_SESSION['mail'];
     $enc = openssl_encrypt	 ($token, 'AES-128-CBC',hex2bin($key),OPENSSL_ZERO_PADDING,hex2bin($iv));
-    setcookie('mail', $enc, 0, '/',  $token, true, true);
+    setcookie('mail', $enc, 0, '/', $host, true, true);
     $mail = mail(htmlspecialchars($_POST["mail"], ENT_QUOTES, 'UTF-8'),"Sign up Token","https://$host?token=$token");
     if (!$mail) {
         echo json_encode(error_get_last()['message']);
