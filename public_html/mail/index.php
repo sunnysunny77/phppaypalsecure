56236e16ea2f761f9f0ca255afb71dcbe9b72ea9
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-08-20 16:46:28 +0800

    up

--- public_html/mail/index.php
+++ public_html/mail/index.php
@@ -3,16 +3,11 @@
 session_start();
 if ($_SESSION['token'] === $_POST['token']) {
     require "../../keys.php";
-
-    $enc = openssl_encrypt	 ($_SESSION['mail'], 'AES-128-CBC',hex2bin($key),OPENSSL_ZERO_PADDING,hex2bin($iv));
-    setcookie('mail', $enc, 0, '/',  $_SERVER['HTTP_HOST'], true, true);
-
-    $to_email = htmlspecialchars($_POST["mail"], ENT_QUOTES, 'UTF-8');
-    $subject = "Sign up Token";
-    $host = $_SERVER['HTTP_HOST']; 
-    $token = $_SESSION['mail']; 
-    $contactus = "https://$host?token=$token";
-    $mail = mail($to_email,$subject,$contactus);
+    $token = $_SERVER['HTTP_HOST'];
+    $token = $_SESSION['mail'];
+    $enc = openssl_encrypt	 ($token, 'AES-128-CBC',hex2bin($key),OPENSSL_ZERO_PADDING,hex2bin($iv));
+    setcookie('mail', $enc, 0, '/',  $token, true, true);
+    $mail = mail(htmlspecialchars($_POST["mail"], ENT_QUOTES, 'UTF-8'),"Sign up Token","https://$host?token=$token");
     if (!$mail) {
         echo json_encode(error_get_last()['message']);
     } else {
