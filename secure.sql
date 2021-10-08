49af17117854fcb056f39c4dbcee4abffb9938f0
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-10-08 13:12:24 +0800

    up

---
+++ secure.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE secure;
+
+USE secure;
+
+CREATE TABLE login ( user varchar(255) NOT NULL, pass varchar(255) NOT NULL , PRIMARY KEY (user) ); 
+
+INSERT INTO login (user, pass) VALUES ("Dan","$2y$15$tdVMkPif5c9YAzq7nmAzkep3ch/qxE2uXwcohYmWGCtQyVUIT6FT2");
