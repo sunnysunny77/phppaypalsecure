1726c0ffb55bf8399a129ae1f52860f32aae8710
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-10-14 17:31:42 +0800

    Update secure.sql

--- secure.sql
+++ secure.sql
@@ -2,6 +2,6 @@ CREATE DATABASE secure;
 
 USE secure;
 
-CREATE TABLE login ( user varchar(255) NOT NULL, pass varchar(255) NOT NULL , PRIMARY KEY (user) ); 
+CREATE TABLE login ( user varchar(255) NOT NULL, pass CHAR(60) NOT NULL , PRIMARY KEY (user) ); 
 
 INSERT INTO login (user, pass) VALUES ("Dan","$2y$15$tdVMkPif5c9YAzq7nmAzkep3ch/qxE2uXwcohYmWGCtQyVUIT6FT2");
